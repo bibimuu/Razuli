6713ef5fa3fbb270c47b6c659ed8bf624a6290a8
平松大悟朗 <hiramatsudaigorou@hiramatsudaigorounoMacBook-Air.local>
2023-10-30 19:49:48 +0900

    install splide

--- assets/custom-slider.js
+++ assets/custom-slider.js
@@ -1,25 +1,22 @@
-$('.custom-slider').slick({
-  autoplay: true,//自動的に動き出すか。初期値はfalse。
-  infinite: true,//スライドをループさせるかどうか。初期値はtrue。
-  slidesToShow: 3,//スライドを画面に3枚見せる
-  slidesToScroll: 3,//1回のスクロールで3枚の写真を移動して見せる
-  prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
-  nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
-  dots: true,//下部ドットナビゲーションの表示
-  responsive: [
-    {
-      breakpoint: 769,//モニターの横幅が769px以下の見せ方
-      settings: {
-        slidesToShow: 2,//スライドを画面に2枚見せる
-        slidesToScroll: 2,//1回のスクロールで2枚の写真を移動して見せる
-      }
-    },
-    {
-      breakpoint: 426,//モニターの横幅が426px以下の見せ方
-      settings: {
-        slidesToShow: 1,//スライドを画面に1枚見せる
-        slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
-      }
-    }
-  ]
-});
+new Splide( '.splide', {
+  classes: {
+		arrows: 'splide__arrows your-class-arrows',
+		arrow : 'splide__arrow your-class-arrow',
+		prev  : 'splide__arrow--prev your-class-prev',
+		next  : 'splide__arrow--next your-class-next',
+  },
+} );
+
+const splide = new Splide( '.splide' );
+
+splide.on( 'pagination:mounted', function ( data ) {
+  // ULエレメントにクラスを追加できます
+  data.list.classList.add( 'splide__pagination--custom' );
+
+  // `items`は、すべてのLI要素とボタン要素を保持しています
+  data.items.forEach( function ( item ) {
+    item.button.textContent = String( item.page + 1 );
+  } );
+} );
+
+splide.mount();
