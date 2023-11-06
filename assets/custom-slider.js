2aa0b2ad99b8f219855b4cb531bf42ed5be38134
平松大悟朗 <hiramatsudaigorou@hiramatsudaigorounoMacBook-Air.local>
2023-11-06 18:52:40 +0900

    made carsel

--- assets/custom-slider.js
+++ assets/custom-slider.js
@@ -1,22 +1,47 @@
-new Splide( '.splide', {
-  classes: {
-		arrows: 'splide__arrows your-class-arrows',
-		arrow : 'splide__arrow your-class-arrow',
-		prev  : 'splide__arrow--prev your-class-prev',
-		next  : 'splide__arrow--next your-class-next',
-  },
-} );
+let count = 0;
+const minCount = 0; // カウントの最小値
+const maxCount = 5; // カウントの最大値（適宜変更）
 
-const splide = new Splide( '.splide' );
+$('.next-box').on('click', function() {
 
-splide.on( 'pagination:mounted', function ( data ) {
-  // ULエレメントにクラスを追加できます
-  data.list.classList.add( 'splide__pagination--custom' );
+  function increaseCount() {
+    if(count == 4){
+      $('.next-box').addClass("disable-button");
+    }
 
-  // `items`は、すべてのLI要素とボタン要素を保持しています
-  data.items.forEach( function ( item ) {
-    item.button.textContent = String( item.page + 1 );
-  } );
-} );
+    if (count < maxCount) {
+      $('.prev-box').removeClass("disable-button");
+      count++; // 変数の値を1増やす。
+      let leftValue = -376 * count;
+      let lineLeftValue = 18 * count;
+      $('.carousel_area__inner').css('left', leftValue + 'px'); // スタイルを設定
+      $('.carousel-moveable-line-style').css('left', lineLeftValue + '%');
+    }
+  }
 
-splide.mount();
+  increaseCount(); // 関数を呼び出す
+
+});
+
+$('.prev-box').on('click', function() {
+  console.log(count)
+  if(count == 0) {
+    $('.prev-box').addClass("disable-button");
+    $('.next-box').removeClass("disable-button");
+  } else {
+    $('.next-box').removeClass("disable-button");
+  }
+
+  function decreaseCount() {
+    if (count > minCount) {
+      count--; // 変数の値を1増やす。
+      let leftValue = -376 * count;
+      let lineLeftValue = 18 * count;
+      $('.carousel_area__inner').css('left', leftValue + 'px'); // スタイルを設定
+      $('.carousel-moveable-line-style').css('left', lineLeftValue + '%');
+    }
+  }
+
+  decreaseCount()
+
+});
